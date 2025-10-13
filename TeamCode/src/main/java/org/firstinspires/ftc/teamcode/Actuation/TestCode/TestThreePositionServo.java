461509a3a0b698f85b4659046e2d4835880595e0
Robert D. Sims III <robert.sims3@gmail.com>
2025-10-12 20:19:38 -0400

    Updated Test Three Position Servo to use FTC Dashboard. Tested and Working.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Actuation/TestCode/TestThreePositionServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Actuation/TestCode/TestThreePositionServo.java
@@ -1,18 +1,27 @@
 package org.firstinspires.ftc.teamcode.Actuation.TestCode;
-
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Actuation.ThreePositionServo;
-
+@Config
 @TeleOp(name="TestThreePositionServo", group="Test")
 public class TestThreePositionServo extends LinearOpMode {
-
-
+    public static double Position1 = 10;
+    public static double Position2 =  50;
+    public static double Position3 = 250;
+    public static double MaxAngle= 300;
+    public static double Offset =0;
     public ThreePositionServo servo;
+    private FtcDashboard dashboard;
     @Override
     public void runOpMode() throws InterruptedException {
-        servo= new ThreePositionServo(hardwareMap,"Test",10,50,250,300,0);
+        servo= new ThreePositionServo(hardwareMap,"Test",Position1 ,Position2 ,Position3 , MaxAngle,Offset);
+        TelemetryPacket packet = new TelemetryPacket();
+        dashboard = FtcDashboard.getInstance();
+
 
         waitForStart();
 
@@ -25,6 +34,22 @@ public class TestThreePositionServo extends LinearOpMode {
             }else if(gamepad1.dpad_left) {
                 servo.setPosition(3);
             }
+
+            packet.put("Position 1 Angle", Position1);
+            packet.put("Position 2 Angle", Position2);
+            packet.put("Position 3 Angle", Position3);
+            packet.put("Max Angle",MaxAngle);
+            packet.put("Offset",Offset);
+            packet.put("Current Servo Position Command", servo.servo.getPosition());
+            dashboard.sendTelemetryPacket(packet);
+
+            telemetry.addData("Servo Position", "Press D-Pad Up/Down/Left");
+            telemetry.addData("P1 Angle", Position1);
+            telemetry.addData("P2 Angle", Position2);
+            telemetry.addData("P3 Angle", Position3);
+            telemetry.addData("Max Angle",MaxAngle);
+            telemetry.addData("Offset",Offset);
+            telemetry.update();
         }
     }
 }
