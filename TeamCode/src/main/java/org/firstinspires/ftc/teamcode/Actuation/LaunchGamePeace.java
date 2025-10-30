e80a8954abd2bd83e9eb43a42561f0f4ddcf6dc0
Evan <evan.thompson515@gmail.com>
2025-10-30 18:57:07 -0400

    Updated Launch Game Piece adding launch all with case

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Actuation/LaunchGamePeace.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Actuation/LaunchGamePeace.java
@@ -6,6 +6,8 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.Actuation.ActuatorControl;
 
+import java.text.BreakIterator;
+
 
 public class LaunchGamePeace {
     private boolean Rebounce = false;
@@ -43,7 +45,68 @@ public class LaunchGamePeace {
 
    }
 
-   public void launchByCode(){
+    public enum SpindleState {
+
+       AllBallsLoaded,
+        TwoBallsLoaded,
+        OneBallLoaded,
+        Empty
+
+
+    }
+
+   SpindleState BallState;
+   public void launch(int pos){
+       actuators.LauncherMotor.StartMotor();
+       opmode.sleep(4000); // Wait for motor to hit speed
+       actuators.spindexercontrol.setPosition(pos);  // increments betweeen all positions
+       opmode.sleep(4000); // Wait for position 1
+       actuators.LaunchKicker.SetFirst();
+       opmode.sleep(4000); // Wait for position 1
+       actuators.LaunchKicker.SetSecond();
+       opmode.sleep(4000); // Wait for position 1
+
+   }
+
+   public void lanchall_case(){
+
+       switch(BallState){
+
+           case AllBallsLoaded:
+               launch(6);
+               BallState = SpindleState.TwoBallsLoaded;
+
+             break;
+
+           case TwoBallsLoaded:
+               launch(5);
+               BallState = SpindleState.OneBallLoaded;
+
+           break;
+
+           case OneBallLoaded:
+
+               launch(4);
+               BallState = SpindleState.Empty;
+
+               break;
+
+           case Empty:
+
+               break;
+
+       }
+
+   }
+
+
+
+
+
+
+
+
+    public void launchByCode(){
 
        // Todo Add code to launch ball by detected code
 
