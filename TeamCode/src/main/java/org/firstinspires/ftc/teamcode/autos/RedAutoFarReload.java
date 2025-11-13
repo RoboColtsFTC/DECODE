2de3231be456bc6d92f496bb9688d459baca3aa6
Evan <evan.thompson515@gmail.com>
2025-11-12 20:32:26 -0500

    Updated Blue and Red Auto Reloads

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/RedAutoFarReload.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.autos;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.drivetrain.MecanumDrive;
+
+@Autonomous(name="RedAutoFarReload", group="Robot")
+public final class RedAutoFarReload extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Pose2d beginPose = new Pose2d(0, 0, Math.toRadians(90));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+
+       Robot robot = new Robot(this,true);
+
+        waitForStart();
+        /* -------------------------------------------------------------------------------------- */
+
+       // beginPose = new Pose2d(0, 0, Math.toRadians(0));
+        drive.localizer.setPose(beginPose);
+        Actions.runBlocking(drive.actionBuilder(beginPose)
+                .strafeToLinearHeading(new Vector2d(0,10), Math.toRadians(90))
+                .turn(Math.toRadians(-25))
+                .stopAndAdd(robot.actuatorcontrol.launchgamepeace.Launch_Auto(.63))
+                .strafeToLinearHeading(new Vector2d(10,-27), Math.toRadians(180))  //position for intake- bottom row
+                .stopAndAdd(robot.actuatorcontrol.loadSpindexer.LoadSpindexer_auto())  //start intake action
+                .strafeToLinearHeading(new Vector2d(34,-27), Math.toRadians(180))  //intake
+                .strafeToLinearHeading(new Vector2d(0,-10), Math.toRadians(-90))  //back to launch position
+                .turn(Math.toRadians(25))
+                .stopAndAdd(robot.actuatorcontrol.launchgamepeace.Launch_Auto(.63)) //launch
+                .strafeToLinearHeading(new Vector2d(5 ,-38), Math.toRadians(0))  //end position
+
+                // new TranslationalVelConstraint(10)
+                .build());
+
+
+
+    }
+}
+//.afterTime(0, robot.actuatorcontrol.launchgamepeace.Launch_Auto())
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/RedAutoNearReload.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.autos;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.drivetrain.MecanumDrive;
+
+@Autonomous(name="RedAutoNearReload", group="Robot")
+public final class RedAutoNearReload extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Pose2d beginPose = new Pose2d(0, 0, Math.toRadians(45));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+
+       Robot robot = new Robot(this,true);
+
+        waitForStart();
+        /* -------------------------------------------------------------------------------------- */
+
+       // beginPose = new Pose2d(0, 0, Math.toRadians(0));
+        drive.localizer.setPose(beginPose);
+        Actions.runBlocking(drive.actionBuilder(beginPose)
+                .strafeToLinearHeading(new Vector2d(-20,-20), Math.toRadians(45))
+                .stopAndAdd(robot.actuatorcontrol.launchgamepeace.Launch_Auto(.50))  //launch
+                .strafeToLinearHeading(new Vector2d(-36,-36), Math.toRadians(180))  //move to intake position
+                .stopAndAdd(robot.actuatorcontrol.loadSpindexer.LoadSpindexer_auto())  //start intake action
+                .strafeToLinearHeading(new Vector2d(0,-36), Math.toRadians(180))  //intake
+                .strafeToLinearHeading(new Vector2d(-20,-20), Math.toRadians(45))  //back to launch position
+                .stopAndAdd(robot.actuatorcontrol.launchgamepeace.Launch_Auto(.50))  //launch
+                .strafeToLinearHeading(new Vector2d(-30 ,-5), Math.toRadians(0))  //end position
+
+                // new TranslationalVelConstraint(10)
+                .build());
+
+
+
+    }
+}
+//.afterTime(0, robot.actuatorcontrol.launchgamepeace.Launch_Auto())
